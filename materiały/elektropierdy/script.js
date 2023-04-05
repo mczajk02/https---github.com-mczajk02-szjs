f55c4abf14ab82065af476299fd4787be96ff1d8
mczajk02 <mczajk02@gmail.com>
2023-04-05 13:49:04 +0200

    drum

--- materiały/elektropierdy/script.js
+++ materiały/elektropierdy/script.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', pierdy())
 
 const keyToSoundMap ={
     q: 'boom',
@@ -12,16 +11,41 @@ const keyToSoundMap ={
     c: 'tom'
 }
 
-
+let recordInProgress = false
+let playInProgress = false
+let recordStartTimestamp = []
+const audioRecord = []
+document.addEventListener('DOMContentLoaded', pierdy())
 
 function pierdy(){
     document.body.addEventListener('keydown', onKeyPress)
+    const recordButton = document.querySelector('#recordButton')
+    recordButton.addEventListener('click', onRecord)
+    const playButton = document.querySelector('#playButton')
+    playButton.addEventListener('click', playRecord)
+
 }
 
 function onKeyPress(e){
     const key = e.key
+    const soundId = keyToSoundMap[key]
     playSound(e.key)
 
+    if(!soundId){
+        return
+    }
+    
+    if(recordInProgress){
+        const timeStamp = Date.now()
+        const sound = {
+            soundId,
+            timeStamp,
+            recordStartTimestamp
+
+        }
+        audioRecord.push(sound)
+        console.log(audioRecord)
+    }
 }
 
 function playSound(key){
@@ -29,4 +53,25 @@ function playSound(key){
     const audio = document.querySelector(audioSelector)
     audio.currentTime = 0
     audio.play()
+}
+
+function onRecord(ev){
+    if(recordInProgress){
+        //akcja stop
+        ev.target.textContent = 'Nagraj'
+    }else {
+    ev.target.textContent = 'Zatrzymaj'        //akcja start
+    }
+
+    recordInProgress =!recordInProgress
+}
+
+function playRecord(ev){
+    if(playInProgress){
+          ev.target.textContent = 'Play'
+    }else {
+    ev.target.textContent = 'STOP'  
+          //akcja start
+    }
+      playInProgress =!playInProgress  //akcja start
 }
